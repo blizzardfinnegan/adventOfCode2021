602211518ee366a0985167f7a337ccf3f3cea5a5
Blizzard Finnegan <blizzardfinnegan@gmail.com>
2021-12-06 12:16:39 -0500

    Day 3 Part 1 Complete

--- day3/day3Part1.cpp
+++ day3/day3Part1.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main (void)
 {
     char currentLine[12];
-    int bitTrue[12];
-    int bitFalse[12];
-    int gammaRateBin[12];
-    int epsilonRateBin[12];
+    int bitTrue[12] = {};
+    int bitFalse[12] = {};
+    int gammaRateBin[12] = {};
+    int epsilonRateBin[12] = {};
     int gammaRateDec = 0;
     int epsilonRateDec = 0;
     int powerConsumption;
@@ -24,32 +24,19 @@ int main (void)
             else cout << "Error!!!";
         }
     }
+
     for(int i=0;i<12;i++)
     {
-        if(bitTrue[i] > bitFalse[i])
-        {
-            gammaRateBin[i] = 1;
-            epsilonRateBin[i] = 0;
-        }
-        else
-        {
-            gammaRateBin[i] = 0;
-            epsilonRateBin[i] = 1;
-        }
+        if(bitTrue[i] > bitFalse[i]) gammaRateBin[i] = 1;
+        else epsilonRateBin[i] = 1;
     }
-    cout << "Gamma Rate (Binary): ";
-    for(int i=0;i<12;i++) cout << gammaRateBin[i];
-
-    cout << "\tEpsilon Rate (Binary): ";
-    for(int i=0;i<12;i++) cout << epsilonRateBin[i];
 
     for(int i=0;i<12;i++)
     {
         gammaRateDec += gammaRateBin[i] * pow(2,11-i);
         epsilonRateDec += epsilonRateBin[i] * pow(2,11-i);
     }
-    cout << "\t Gamma Rate (Decimal): " << gammaRateDec << "\tEpsilon Rate (Decimal): " << epsilonRateDec;
     powerConsumption = gammaRateDec * epsilonRateDec;
-    cout << "Power Consumption: " << powerConsumption;
+    cout << "\nPower Consumption: " << powerConsumption;
     return 0;
 }
